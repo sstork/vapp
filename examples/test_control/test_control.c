6008222ee8b67e82cbf0d8604d08bcb7960ef6c4
sven.stork.CMU <sven.stork.CMU@4af3187c-be57-11de-a8ef-132414f8baa6>
2009-10-23 00:51:30 +0000

    - add test for control mechanism

---
+++ examples/test_control/test_control.c
@@ -0,0 +1,66 @@
+// -*- c-basic-offset : 4 -*-
+/*
+ * Copyright (c) 2009, Antony Gitter, Sven Stork
+ * All rights reserved.
+ 1;1613;0c*
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <VAPPControl.h>
+
+#define SIZE 10
+
+void fib(int *buf)
+{
+    buf[0] = 0;
+}
+
+
+int main(void){
+  int value;
+  int *buffer;
+  int i = 0;
+
+  VAPPTraceOn(VAPP_MEM_ACCESS | VAPP_FN_CALL | VAPP_ALLOC_FREE);
+
+  buffer = (int*)malloc(SIZE*SIZE*sizeof(int));
+  
+  printf("HelloWorld\n");
+
+  
+  for ( i = 0 ; i < SIZE*SIZE ; i++ ) {
+      buffer[i] = value;
+  }
+
+  fib(buffer);
+  i = buffer[0];
+  free(buffer);
+
+  VAPPTraceOff(VAPP_ALLOC_FREE);
+  VAPPTraceOff(VAPP_MEM_ACCESS | VAPP_FN_CALL);
+
+  return i;
+}
