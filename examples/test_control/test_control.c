8bf1bed91a7c53be8b811fa1ae6122abed140816
sven.stork.CMU <sven.stork.CMU@4af3187c-be57-11de-a8ef-132414f8baa6>
2009-11-11 21:11:49 +0000

    - fix comment

--- examples/test_control/test_control.c
+++ examples/test_control/test_control.c
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2009, Anthony Gitter, Sven Stork
  * All rights reserved.
- 1;1613;0c*
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
