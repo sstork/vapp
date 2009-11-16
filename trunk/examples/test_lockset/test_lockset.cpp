b5d1dc15fe5b832cac5e236f22902c3dc26843e0
Anthony.Gitter@gmail.com <Anthony.Gitter@gmail.com@4af3187c-be57-11de-a8ef-132414f8baa6>
2009-11-16 06:52:58 +0000

    Bug fixes in test_lockset.cpp

--- trunk/examples/test_lockset/test_lockset.cpp
+++ trunk/examples/test_lockset/test_lockset.cpp
@@ -7,39 +7,27 @@
 
 using namespace std;
 
-
-typedef struct {
-    pthread_mutex_t lock;
-    int value;
-} counter_t;
-
 int main (void) {
     pthread_mutex_t lock1, lock2;
-    pthread_mutex_init(lock1);
-    pthread_mutex_init(lock2);
+    pthread_mutex_init(&lock1, NULL);
+    pthread_mutex_init(&lock2, NULL);
 
     int *a = (int*)malloc(sizeof(int));
     int *b = (int*)malloc(sizeof(int));
     int *c = (int*)malloc(sizeof(int));
     int *d = (int*)malloc(sizeof(int));
 
-    cout << "a: " << a << endl;
-    cout << "b: " << b << endl;
-    cout << "c: " << c << endl;
-    cout << "d: " << c << endl;
-
-
-    pthread_mutex_lock(lock1);
-    a++;
-    b++;
-    pthread_mutex_lock(lock2);
-    a++;
-    b++;
-    c++;
-    pthread_mutex_unlock(lock1);
-    a++;
-    c++;
-    pthread_mutex_unlock(lock2);
+    pthread_mutex_lock(&lock1);
+    (*a)++;
+    (*b)++;
+    pthread_mutex_lock(&lock2);
+    (*a)++;
+    (*b)++;
+    (*c)++;
+    pthread_mutex_unlock(&lock1);
+    (*a)++;
+    (*c)++;
+    pthread_mutex_unlock(&lock2);
 
     free(a);
     free(c);
